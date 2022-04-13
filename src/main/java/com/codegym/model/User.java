582a1550e4610f6ac1fb25bd9e92e3043d52a33d
unknown <sangnhanma14@gmail.com>
2022-04-13 10:05:40 +0700

    security

---
+++ src/main/java/com/codegym/model/User.java
@@ -0,0 +1,33 @@
+package com.codegym.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "user_role")
+    private List<Role> roles;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
