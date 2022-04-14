253334403183642638353f19e4de23d4361b18d3
dangtu98 <bdt1998gmail.com>
2022-04-14 14:24:50 +0700

    delete author,genre,publish

--- src/main/java/com/codegym/repository/IGenreRepository.java
+++ src/main/java/com/codegym/repository/IGenreRepository.java
@@ -2,11 +2,17 @@ package com.codegym.repository;
 
 import com.codegym.model.Genre;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IGenreRepository extends PagingAndSortingRepository<Genre, Long> {
    Iterable<Genre> findByNameContaining(String name);
+
+   @Modifying
+   @Query(value = "call delete_genre(?1)",nativeQuery = true)
+   void deleteGenre(Long id);
 
 }
--- src/main/java/com/codegym/repository/IPublisherRepository.java
+++ src/main/java/com/codegym/repository/IPublisherRepository.java
@@ -1,6 +1,8 @@
 package com.codegym.repository;
 
 import com.codegym.model.Publisher;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface IPublisherRepository extends PagingAndSortingRepository<Publisher,Long> {
     Iterable<Publisher> findByNameContaining(String name);
 
+    @Modifying
+    @Query(value = "call delete_publisher(?1)",nativeQuery = true)
+    void deletePublisher(Long id);
 }
